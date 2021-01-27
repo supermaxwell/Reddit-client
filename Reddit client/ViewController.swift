858557bd5871d6c080ab9da67685ff1548fe63f3
Max <supermaxwell@gmail.com>
2021-01-27 16:30:30 +0200

    3 cells -not URLsession -static

--- Reddit client/ViewController.swift
+++ Reddit client/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+
+
+    @IBOutlet weak var tableView: UITableView!
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    //MARK: - TABLEVIEW DELEGATE & DATASOURSE
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
 
-}
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath)
+
+        
+        return cell
+    }
+
+    }
+
 
